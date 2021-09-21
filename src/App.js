0e33639423d64750325e59fd5b3bfc8e7ff7334c
shopinDan <shopin.da@justiceteam-it.com>
2021-09-21 11:30:41 +0300

    useEffect tutuor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-import {useState} from "react";
+import { useEffect, useState } from "react";
 
 function App() {
     const [count, setCount] = useState(0);
+    const [res, setRes] = useState(5)
+    useEffect(() => {
+        console.log('first')
+    }, [])
+    useEffect(() => {
+        console.log('update')
+    }, [count])
+    useEffect(() => {
+        return (() => {
+            console.log('will unmount')
+        })
+    }, [res])
+    useEffect(() => {
+        console.log('UPDATE!!!')
+    })
   return (
     <div className="App">
         <h1 className={count >= 0? "positive" : "negative"}>{count}</h1>
         <div className="button__wrapper">
             <button onClick={()=>setCount(count+1)}>+</button>
-            <button onClick={()=>setCount(count-1)}>-</button>
+            <button onClick={()=>setCount(count - 1)}>-</button>
+            <button onClick={()=>setRes(res * 0)}>*</button>
+            {res > 0 && res}
         </div>
 
     </div>
